956905801ad1caae3d3e539c677b06e1da91ee78
avborisov <borisov.norilsk@gmail.com>
2020-06-27 13:58:34 +0500

    initial commit

---
+++ src/main/java/ru/alfabattle/borisov/alfabattlepromo/AlfabattlePromoApplication.java
@@ -0,0 +1,13 @@
+package ru.alfabattle.borisov.alfabattlepromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfabattlePromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfabattlePromoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/alfabattle/borisov/alfabattlepromo/AlfabattlePromoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alfabattle.borisov.alfabattlepromo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfabattlePromoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
